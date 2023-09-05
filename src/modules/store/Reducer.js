8546a90f102d8b68b116ca4c08630003a4488b83
Sanches-23 <pasechnik23.2002@gmail.com>
2023-09-05 18:59:40 +0300

    hw21

---
+++ src/modules/store/Reducer.js
@@ -0,0 +1,25 @@
+const initState = {
+    loading: false,
+    count: 0,
+    user: {
+        name: "AAA",
+        age: 23,
+    },
+}
+
+export const reducer = (state = initState, action) => {
+    switch (action.type) {
+        case 'plus': {
+            return {...state, count: state.count + 1};
+        }
+        case 'minus': {
+            return {...state, count: state.count + 1};
+        }
+        case 'reset': {
+            // return {...initState, count: initState.count};
+            return {...state, count: 0};
+        }
+        default:
+            return state;
+    }
+}
---
+++ src/modules/store/Store.js
@@ -0,0 +1,7 @@
+import { legacy_createStore as createStore } from "redux";
+import { reducer } from './Reducer';
+
+const store = createStore(reducer);
+
+export default store;
+

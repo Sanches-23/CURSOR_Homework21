f602e1e38c5d0b9866ac2662ba209c72dd01e5e5
Sanches-23 <pasechnik23.2002@gmail.com>
2023-09-13 22:17:37 +0300

    init

--- src/modules/pages/WritePost.js
+++ src/modules/pages/WritePost.js
@@ -1,17 +1,32 @@
 // import AppStyle from "../style/App.module.css";
 // import {Provider} from "react-redux";
-import {useSelector} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
 import {countSelector} from "../store/Selector";
 
 function WritePost() {
 
     const count = useSelector(countSelector);
+        const dispatch = useDispatch();
+        const handlePlus = () => {
+            const action = {
+                type: "plus",
+                payload: 2,
+            }
+            dispatch(action)
+        }
 
         return (
         <>
-                <h2>WritePost</h2>
             {/*<Provider>*/}
-                <h3>AAA: {count}</h3>
+            <h2>WritePost</h2>
+            <h3>AAA: {count}</h3>
+            <div>
+                <button  onClick={handlePlus}>+</button>
+                {/*<button  onClick={handleReset}>Reset</button>*/}
+                {/*<button  onClick={handleMinus}>-</button>*/}
+            </div>
+
+
 
             {/*</Provider>*/}
         </>
--- src/modules/store/Reducer.js
+++ src/modules/store/Reducer.js
@@ -10,7 +10,7 @@ const initState = {
 export const reducer = (state = initState, action) => {
     switch (action.type) {
         case 'plus': {
-            return {...state, count: state.count + 1};
+            return {...state, count: state.count + action.payload};
         }
         case 'minus': {
             return {...state, count: state.count + 1};
